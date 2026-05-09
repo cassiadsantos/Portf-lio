8221fd8b09b2af0fa3499fa012cfda4e19f43423
Aluno <cassia.d.santos6@edu.senai.br>
2026-05-09 17:33:05 -0300

    Primeira Versão do Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+const otaoMensagem = document.getElementById("botaoMensagem")
+const mensagem = document.getElementById("mensagem")
+
+function alterarTexto() {
+    //mensagem.textContent = "Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript"
+    alert("Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript")
+}
+
+
